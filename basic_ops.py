be31634dfc4477bc9410c0daa23f99500603f6e8
Ben <benalexander415@gmail.com>
2023-04-05 16:46:00 -0400

    Updates to U_x for matrix

--- basic_ops.py
+++ basic_ops.py
@@ -9,16 +9,19 @@ import cmath
 # Defining Pauli spin mats
 SIGMA_1 = np.array[[complex(0), complex(1, 0)], [complex(1, 0), complex(0)]]
 SIGMA_2 = np.array[[complex(0), complex(0, -1)], [complex(0, 1), complex(0)]]
-SIGMA_2 = np.array[[complex(1), complex(0)], [complex(0), complex(-1)]]
+SIGMA_3 = np.array[[complex(1), complex(0)], [complex(0), complex(-1)]]
 def U_x(state, target_particle):
-    # NOT operator acting on target particle
+    # NOT operator acting on target particle (particle #)
     # n = number of particles in system
     n = len(state)//2
     # Construct matrix:
     mat = [1]
-    for _ in range(n-1):
-        mat = np.kron(mat, np.identity(2))
-    mat = np.kron(mat, )
+    for i in range(n):
+        if i != target_particle:
+            mat = np.kron(mat, np.identity(2))
+        else:
+            mat = np.kron(mat, SIGMA_1)
+
     return state
 
 
