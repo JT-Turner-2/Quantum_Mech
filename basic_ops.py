7b7bc35c6e8204ef9bde9e02b207b151a3649d30
JT-Turner-2 <123335517+JT-Turner-2@users.noreply.github.com>
2023-04-05 16:12:05 -0400

    JT base_ops

---
+++ basic_ops.py
@@ -0,0 +1,6 @@
+import numpy as np
+import astropy
+from astropy import units as u
+from astropy import constants
+import cmath
+#idk if cmath is installed in my enviroment state is array of complext
