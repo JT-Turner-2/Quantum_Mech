7fabf566ef8062ae93c54ff51c960d184f362d3e
JT-Turner-2 <123335517+JT-Turner-2@users.noreply.github.com>
2023-04-09 13:30:16 -0400

    plans for how to measure the sz of the particle system

---
+++ measure.py
@@ -0,0 +1,5 @@
+import numpy as np
+import sympy as sym
+def measure(state):
+# measure the state of eigenstate of Sz of all particles. Return 0,1 where 0 is pluz z and 1 is minus z
+# weighted probability
