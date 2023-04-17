91b21122b82f6232f7671da05551e3427de1eda9
JT-Turner-2 <123335517+JT-Turner-2@users.noreply.github.com>
2023-04-17 13:15:53 -0400

    Updated to remove the state size thing

--- actual_diffuser.py
+++ actual_diffuser.py
@@ -1,8 +1,6 @@
 from basic_ops import *
 from function_helpers import measure, generate_basis_state, run_everything
-def diffuser (oracle,n):
-    state_size=n+1 # check to make sure this is correct
-    psi = generate_basis_state(1, state_size)
+def diffuser (oracle,n,psi):
     # Step 1- Hadamard
     psi=run_everything(psi,U_H())
     #Step 2 - U_z
