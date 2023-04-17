8dbb2784a6e6da40d853016bf4d39a09f6ce5b1b
JT-Turner-2 <123335517+JT-Turner-2@users.noreply.github.com>
2023-04-17 13:18:15 -0400

    grover's algorithm. Untested

---
+++ grovers_algorithm.py
@@ -0,0 +1,15 @@
+from basic_ops import *
+from function_helpers import measure, generate_basis_state, run_everything
+from actual_diffuser import*
+def grovers_algorithm (oracle, n):
+    state_size = n + 1  # check to make sure this is correct
+    psi = generate_basis_state(1, state_size)
+    #step 1 Hadamard
+    psi=run_everything(psi,U_H())
+    #step 2 - Oracle
+    psi=oracle(psi)
+    #step 3 - diffuser
+    psi=diffuser(oracle,n,psi)
+    #Step 4 - measure
+    measurement=measure(psi)
+    return measurement
