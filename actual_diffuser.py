3cfba00a79584d6e735fd15db5ca89946ff0681e
JT-Turner-2 <123335517+JT-Turner-2@users.noreply.github.com>
2023-04-17 19:22:30 -0400

    should work for 3+ bits
    Need to remember what a controlled z gate is but I think that's the standard oracle for grovers

--- actual_diffuser.py
+++ actual_diffuser.py
@@ -3,10 +3,14 @@ from function_helpers import measure, generate_basis_state, run_everything
 def diffuser (oracle,n,psi):
     # Step 1- Hadamard
     psi=run_everything(psi,U_H())
-    #Step 2 - U_z
-    psi=run_everything(psi,U_z())
-    # Step 3 - oracle
+    #Step 2 - U_x
+    psi=run_everything(psi,U_x())
+    # Step 3 - oracle should be the controlled z gate
     psi=oracle(psi)
-    #Step 4- Hadamard
+    #step 4 - U_x
+    psi=run_everything(psi,U_x())
+    #Step 5- Hadamard
     psi=run_everything(psi,U_H())
     return psi
+#todo make sure the controlled z gzte makes sense
+# DO NOT LOOP IN HERE!
