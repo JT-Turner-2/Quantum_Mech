d7c183f670739b312ff268061f34332833fd0e4c
Ben <benalexander415@gmail.com>
2023-04-20 20:57:03 -0400

    Grover's algorithm seems to work, at least somewhat; may want to build more from fundamental gates and/or modify behavior for small size system

--- grovers_algorithm.py
+++ grovers_algorithm.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from basic_ops import *
 from function_helpers import measure, generate_basis_state, run_everything
 
@@ -12,19 +14,18 @@ def grover_algorithm (oracle, n):
     dim = len(psi) # also known as big N in literature
 
     # Now apply oracle and then diffusion operator number of steps times
-    number_of_steps = int(np.ceil(np.sqrt(dim)))
+    number_of_steps = int(np.sqrt(dim)) # TODO May need to modify this calculation with a constant
     for _ in range(number_of_steps):
         psi = oracle(psi)
-        psi = diffuser(oracle, n, psi)
-        print(psi)
-    #Step 4 - measure
+        psi = diffuser(oracle, psi)
+    # Now measure the state; should give desired output with high probability
     measurement = measure(psi)
     return measurement
 
 
 def generate_oracle(answer):
     # Function to generate an oracle function.
-    # answer is the answer of the oracle (ie: which element is the one we are searching for)
+    # answer is the answer of the oracle (ie: index of element we are searching for)
     def oracle_func(state):
         dim = len(state)
         # first, do some error checking:
@@ -37,20 +38,27 @@ def generate_oracle(answer):
     return oracle_func
 
 
-def diffuser (oracle,n,psi):
+def diffuser(oracle, psi):
     # Step 1- Hadamard
     psi = run_everything(psi, U_H)
     # Step 2 - U_x
-    psi = run_everything(psi, U_x)
+    # psi = run_everything(psi, U_x)
     # Step 3 - oracle should be the controlled z gate
-    psi = oracle(psi)
+    psi = z_0(psi)
     # Step 4 - U_x
-    psi = run_everything(psi, U_x)
+    # psi = run_everything(psi, U_x)
     # Step 5- Hadamard
     psi = run_everything(psi, U_H)
     return psi
 
 
-oracle_1 = generate_oracle(0)
-m = grover_algorithm(oracle_1, 3)
-print(m)
+def z_0(state):
+    # z_0 gate (as described in Wright)
+    dim = len(state)
+    mat = -1 * np.identity(dim)
+    mat[0, 0] = 1
+    return mat @ state
+
+# oracle_1 = generate_oracle(1)
+# m = grover_algorithm(oracle_1, 6)
+# print(m)
