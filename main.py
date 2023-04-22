82b2f4ec38a5917bcf3d8bd03dbb682934ae0353
JT-Turner-2 <123335517+JT-Turner-2@users.noreply.github.com>
2023-04-22 13:12:06 -0400

    classical implementation complete

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # Main script for our quantum computer
-
+'''
+def generate_basis_state(k, n):
+    # Generate kth basis state of comp basis for n particle system
+    state = np.zeros(2**n)
+    state[k] = 1
+    return state
+    '''
 # Module imports
 import numpy as np
 
@@ -11,4 +17,18 @@ from deutsch_jorza.py import*
 #classical implementation
 def classical_implementation(state):
   counter=0
+  x=0
+  check=state[x]
+  if check==1:
+    counter=counter+1
+    return counter
+  else:
+    counter=counter+1
+    x=x+1
+    if x>len(state):
+      print("no successful matches")
+      return -1
+  
+  
+  #create the graph bit
   
