65e274cf3a49cf6def8d08556de988582c525aa2
JT-Turner-2 <123335517+JT-Turner-2@users.noreply.github.com>
2023-04-22 13:03:35 -0400

    classic implementation in progress

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ import numpy as np
 # Other script imports
 from basic_ops import *
 from function_helpers import *
+from grovers_algorithim import *
+from deutsch_jorza.py import*
+#classical implementation
+def classical_implementation(state):
+  counter=0
+  
